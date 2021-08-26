c41b63774265f0ef4ccfc00696d150664b30a7ab
Edward Zhou <Edward_mjz@hotmail.com>
2021-08-27 09:02:23 +1000

    high-scores

---
+++ high-scores/high_scores.py
@@ -0,0 +1,13 @@
+def latest(scores):
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    scores.sort(reverse=True)
+    top_three_scores = scores[0:3]
+
+    return top_three_scores
