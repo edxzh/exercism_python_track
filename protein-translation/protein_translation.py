0be0d18d52723e6c411594171f59690525521809
Edward Zhou <Edward_mjz@hotmail.com>
2021-08-31 18:41:04 +1000

    protein_translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,29 @@
+import itertools
+import re
+
+codon_proteins_dict = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'
+}
+
+
+def proteins(strand):
+    codons = re.findall(r'\w{3}', strand)
+    proteins = (codon_proteins_dict[codon] for codon in codons)
+
+    return list(itertools.takewhile(lambda protein: protein != 'STOP', proteins))
