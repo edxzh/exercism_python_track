d7c55a601571dfb881b4aaf2b8c6ba36d32a089b
Edward Zhou <Edward_mjz@hotmail.com>
2021-09-03 23:04:08 +1000

    Update series

--- series/series.py
+++ series/series.py
@@ -2,6 +2,7 @@ def slices(series, length):
     if length > len(series) or length < 1:
         raise ValueError("Error")
 
-    result = list(series[idx : idx + length] for idx, val in enumerate(series) if idx + length <= len(series))
+    result = [series[idx: idx + length]
+              for idx, val in enumerate(series) if idx + length <= len(series)]
 
     return result
