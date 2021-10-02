1e47529260db90cb0d4ebfaf7f101c4a6bdeca6b
Edward Zhou <Edward_mjz@hotmail.com>
2021-10-02 22:16:52 +1000

    difference-of-squares

---
+++ difference-of-squares/difference_of_squares.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=aI0M4XRiz4I
+def square_of_sum(number):
+    return (number * (number + 1) / 2) ** 2
+
+
+def sum_of_squares(number):
+    return number * (number + 1) * (2 * number + 1) / 6
+
+
+def difference_of_squares(number):
+    return number * (number + 1) * (3 * number ** 2 - number - 2) / 12
